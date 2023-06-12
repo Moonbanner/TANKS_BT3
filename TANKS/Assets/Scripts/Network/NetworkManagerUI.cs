00e36e736461ba602e2b2d4a7da380f7ba6622c3
Moonbanner <62237431+Moonbanner@users.noreply.github.com>
2023-06-12 08:06:22 +0700

    Connected Players Count
    Added [0] PLAYERS CONNECTED text on bottom left of the screen UI that shows what it says

--- TANKS/Assets/Scripts/Network/NetworkManagerUI.cs
+++ TANKS/Assets/Scripts/Network/NetworkManagerUI.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NetworkManagerUI : MonoBehaviour
+public class NetworkManagerUI : NetworkBehaviour    
 {
     [SerializeField] private Button serverbtn;
     [SerializeField] private Button hostbtn;
     [SerializeField] private Button clientbtn;
+    [SerializeField] private TextMeshProUGUI player_NumOfText;
+
+    private NetworkVariable<int> numOfPlayers = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone);
 
     private void Awake()
     {
@@ -29,4 +33,10 @@ public class NetworkManagerUI : MonoBehaviour
             );
     }
 
+    private void Update()
+    {
+        player_NumOfText.text = numOfPlayers.Value.ToString() + " CONNECTED PLAYERS";
+        if (!IsServer) return;
+        numOfPlayers.Value = NetworkManager.Singleton.ConnectedClients.Count;
+    }
 }
